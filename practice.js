4843769de5d4f08ce2125d3221b40a8e97f968e3
Abner Suniga <abner.suniga@hotmail.com>
2018-06-03 17:53:44 -0300

    All 7 problems solved

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr) {
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr) {
+  return arr[arr.length-1];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  family.forEach(element => {
+    alert(element);
+  });
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for (let i = letters.length-1; i >= 0 ; i--) {
+    const element = letters[i];    
+    alert(element);    
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +91,11 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums) {
+  return nums.filter(function (element){
+    return element % 2 == 0;
+  });
+}
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
@@ -95,8 +112,16 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
-
-
+function divider(numbersArray) {
+  var array = []
+  array[0] = numbersArray.filter(function (element){
+    return element % 2 == 0;
+  });
+  array[1] = numbersArray.filter(function (element){
+    return element % 2 == 1;
+  });
+  return array;
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -116,7 +141,16 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
-
+function finder(array) {
+  var randomNumber = getRandomArbitrary();
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if(element == randomNumber){
+      return true;
+    }    
+  }
+  return false;
+}
 
 
 ////////// PROBLEM 8 //////////
